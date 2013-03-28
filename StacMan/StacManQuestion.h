bbe6dbd1a6ca1bf132efcb15b0942d651c743505
Kevin Montrose <kevin.montrose@gmail.com>
2013-03-28 14:16:46 -0400

    Reflection POC on StacManQuestion

--- StacMan/StacManQuestion.h
+++ StacMan/StacManQuestion.h
@@ -9,5 +9,62 @@
 #import <Foundation/Foundation.h>
 
 @interface StacManQuestion : NSObject
+@property NSNumber* acceptedAnswerId;
+
+@property NSNumber* answerCount;
+
+@property NSArray* answers;
+
+@property NSString* body;
+
+@property NSNumber* bountyAmount;
+
+@property NSDate* bountyClosesDate;
+
+@property NSDate* closedDate;
+
+@property NSString* closedReason;
+
+@property NSArray* comments;
+
+@property NSDate* communityOwnedDate;
+
+@property NSDate* creationDate;
+
+@property NSNumber* downVoteCount;
+
+@property NSNumber* favoriteCount;
+
+@property NSNumber* isAnswered;
+
+@property NSDate* lastActivityDate;
+
+@property NSDate* lastEditDate;
+
+@property NSString* link;
+
+@property NSDate* lockedDate;
+
+//@property StacManMigrationInfo* migratedFrom;
+
+//@property StacManMigrationInfo* migratedTo;
+
+//@property StacManShallowUser* owner;
+
+@property NSDate* protectedDate;
+
+@property NSNumber* questionId;
+
+@property NSNumber* score;
+
+@property NSArray* tags;
+
+@property NSString* title;
+
+@property NSNumber* upVoteCount;
+
+@property NSNumber* viewCount;
+
++(StacManQuestion*)parse:(NSDictionary*)dict;
 +(NSArray*)parseArray:(NSArray*)json;
 @end
--- StacMan/StacManQuestion.m
+++ StacMan/StacManQuestion.m
@@ -7,10 +7,94 @@
 //
 
 #import "StacManQuestion.h"
+#import <objc/runtime.h>
 
 @implementation StacManQuestion
+
+@synthesize acceptedAnswerId;
+@synthesize answerCount;
+@synthesize answers;
+@synthesize body;
+@synthesize bountyAmount;
+@synthesize bountyClosesDate;
+@synthesize closedDate;
+@synthesize closedReason;
+@synthesize comments;
+@synthesize communityOwnedDate;
+@synthesize creationDate;
+@synthesize downVoteCount;
+@synthesize favoriteCount;
+@synthesize isAnswered;
+@synthesize lastActivityDate;
+@synthesize link;
+@synthesize lockedDate;
+@synthesize lastEditDate;
+@synthesize protectedDate;
+@synthesize questionId;
+@synthesize score;
+@synthesize tags;
+@synthesize title;
+@synthesize viewCount;
+@synthesize upVoteCount;
+
+NSString* ToKey(const char* str)
+{
+    NSMutableString* ret = [NSMutableString string];
+    
+    CFStringRef cfStr = CFStringCreateWithCString(NULL, str, kCFStringEncodingUTF8);
+    
+    for(int i = 0; i < CFStringGetLength(cfStr); i++)
+    {
+        UniChar uc = CFStringGetCharacterAtIndex(cfStr, i);
+        
+        NSString* asStr = [NSString stringWithCharacters:&uc length:1];
+        
+        if([[NSCharacterSet uppercaseLetterCharacterSet] characterIsMember: uc])
+        {
+            [ret appendString:@"_"];
+            [ret appendString:[asStr lowercaseString]];
+        }
+        else
+        {
+            [ret appendString:asStr];
+        }
+    }
+    
+    return ret;
+}
+
++(StacManQuestion*)parse:(NSDictionary*)dict
+{
+    StacManQuestion* ret = [StacManQuestion alloc];
+    
+    Class c = [StacManQuestion class];
+    unsigned int count;
+    objc_property_t* props = class_copyPropertyList(c, &count);
+    
+    for(unsigned int i = 0; i < count; i++)
+    {
+        const char* name = property_getName(props[i]);
+        NSString* propName = [NSString stringWithCString:name encoding:kCFStringEncodingUTF8];
+        NSString* asKey = ToKey(name);
+        
+        id inDict = [dict valueForKey:asKey];
+        if(inDict == nil) continue;
+        
+        [ret setValue:inDict forKey:propName];
+    }
+    
+    return ret;
+}
+
 +(NSArray*)parseArray:(NSArray*)json
 {
-    return nil;
+    NSMutableArray* ret = [NSMutableArray arrayWithCapacity:json.count];
+    
+    for(int i = 0; i < json.count;i++)
+    {
+        [ret addObject:[StacManQuestion parse:[json objectAtIndex:i]]];
+    }
+    
+    return ret;
 }
 @end
