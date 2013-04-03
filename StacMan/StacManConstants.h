94e42000150dee1f4314325fc83624f214ccaac8
Kevin Montrose <kevin.montrose@gmail.com>
2013-04-03 10:15:16 -0400

    defining a ton of constants

--- StacMan/StacManConstants.h
+++ StacMan/StacManConstants.h
@@ -8,14 +8,166 @@
 
 #import <Foundation/Foundation.h>
 
-// User types
-FOUNDATION_EXPORT NSString *const StacManUnregisteredUserType;
-FOUNDATION_EXPORT NSString *const StacManRegisteredUserType;
-FOUNDATION_EXPORT NSString *const StacManModeratorUserType;
-FOUNDATION_EXPORT NSString *const StacManDoesNotExistUserType;
+// Answer sorts
+FOUNDATION_EXPORT NSString *const StacManActivityAnswerSort;
+FOUNDATION_EXPORT NSString *const StacManCreationAnswerSort;
+FOUNDATION_EXPORT NSString *const StacManVotesAnswerSort;
+
+// Badge All sorts
+FOUNDATION_EXPORT NSString *const StacManRankBadgeAllSort;
+FOUNDATION_EXPORT NSString *const StacManNameBadgeAllSort;
+FOUNDATION_EXPORT NSString *const StacManTypeBadgeAllSort;
+
+// Badge ranks
+FOUNDATION_EXPORT NSString *const StacManGoldBadgeRank;
+FOUNDATION_EXPORT NSString *const StacManSilverBadgeRank;
+FOUNDATION_EXPORT NSString *const StacManBronzeBadgeRank;
+
+// Badge sorts
+FOUNDATION_EXPORT NSString *const StacManRankBadgeSort;
+FOUNDATION_EXPORT NSString *const StacManNameBadgeSort;
+
+// Badge types
+FOUNDATION_EXPORT NSString *const StacManNamedBadgeType;
+FOUNDATION_EXPORT NSString *const StacManTagBasedBadgeType;
+
+// Badge User sorts
+FOUNDATION_EXPORT NSString *const StacManRankBadgeUserSort;
+FOUNDATION_EXPORT NSString *const StacManNameBadgeUserSort;
+FOUNDATION_EXPORT NSString *const StacManTypeBadgeUserSort;
+FOUNDATION_EXPORT NSString *const StacManAwardedBadgeUserSort;
+
+// Comment sorts
+FOUNDATION_EXPORT NSString *const StacManCreationCommentSort;
+FOUNDATION_EXPORT NSString *const StacManVotesCommentSort;
+
+// Event types
+FOUNDATION_EXPORT NSString *const StacManQuestionPostedEventType;
+FOUNDATION_EXPORT NSString *const StacManAnswerPostedEventType;
+FOUNDATION_EXPORT NSString *const StacManCommentPostedEventType;
+FOUNDATION_EXPORT NSString *const StacManPostEditedEventType;
+FOUNDATION_EXPORT NSString *const StacManUserCreatedEventType;
+
+// Filter types
+FOUNDATION_EXPORT NSString *const StacManSafeFilterType;
+FOUNDATION_EXPORT NSString *const StacManUnsafeFilterType;
+FOUNDATION_EXPORT NSString *const StacManInvalidFilterType;
+
+// InboxItem types
+FOUNDATION_EXPORT NSString *const StacManCommentInboxItemType;
+FOUNDATION_EXPORT NSString *const StacManChatMessageInboxItemType;
+FOUNDATION_EXPORT NSString *const StacManNewAnswerInboxItemType;
+FOUNDATION_EXPORT NSString *const StacManCareersMessageInboxItemType;
+FOUNDATION_EXPORT NSString *const StacManCareersInvitationInboxItemType;
+FOUNDATION_EXPORT NSString *const StacManMetaQuestionInboxItemType;
+FOUNDATION_EXPORT NSString *const StacManPostNoticeInboxItemType;
+FOUNDATION_EXPORT NSString *const StacManModeratorMessageInboxItemType;
+
+// Orders
+FOUNDATION_EXPORT NSString *const StacManDescendingOrder;
+FOUNDATION_EXPORT NSString *const StacManAscendingOrder;
+
+// Post sorts
+FOUNDATION_EXPORT NSString *const StacManActivityPostSort;
+FOUNDATION_EXPORT NSString *const StacManCreationPostSort;
+FOUNDATION_EXPORT NSString *const StacManVotesPostSort;
+
+// Post types
+FOUNDATION_EXPORT NSString *const StacManQuestionPostType;
+FOUNDATION_EXPORT NSString *const StacManAnswerPostType;
+
+// Question All sorts
+FOUNDATION_EXPORT NSString *const StacManActivityQuestionAllSort;
+FOUNDATION_EXPORT NSString *const StacManVotesQuestionAllSort;
+FOUNDATION_EXPORT NSString *const StacManCreationQuestionAllSort;
+FOUNDATION_EXPORT NSString *const StacManHotQuestionAllSort;
+FOUNDATION_EXPORT NSString *const StacManWeekQuestionAllSort;
+FOUNDATION_EXPORT NSString *const StacManMonthQuestionAllSort;
+
+// Question Favorite sorts
+FOUNDATION_EXPORT NSString *const StacManActivityQuestionFavoriteSort;
+FOUNDATION_EXPORT NSString *const StacManCreationQuestionFavoriteSort;
+FOUNDATION_EXPORT NSString *const StacManVotesQuestionFavoriteSort;
+FOUNDATION_EXPORT NSString *const StacManAddedQuestionFavoriteSort;
+
+// Question Related sorts
+FOUNDATION_EXPORT NSString *const StacManActivityQuestionRelatedSort;
+FOUNDATION_EXPORT NSString *const StacManCreationQuestionRelatedSort;
+FOUNDATION_EXPORT NSString *const StacManVotesQuestionRelatedSort;
+FOUNDATION_EXPORT NSString *const StacManRankQuestionRelatedSort;
+
+// Question Search sorts
+FOUNDATION_EXPORT NSString *const StacManActivityQuestionSearchSort;
+FOUNDATION_EXPORT NSString *const StacManCreationQuestionSearchSort;
+FOUNDATION_EXPORT NSString *const StacManVotesQuestionSearchSort;
+FOUNDATION_EXPORT NSString *const StacManRelevanceQuestionSearchSort;
+
+// Question sorts
+FOUNDATION_EXPORT NSString *const StacManActivityQuestionSort;
+FOUNDATION_EXPORT NSString *const StacManCreationQuestionSort;
+FOUNDATION_EXPORT NSString *const StacManVotesQuestionSort;
+
+// QuestionTimeline types
+FOUNDATION_EXPORT NSString *const StacManQuestionQuestionTimelineType;
+FOUNDATION_EXPORT NSString *const StacManAnswerQuestionTimelineType;
+FOUNDATION_EXPORT NSString *const StacManCommentQuestionTimelineType;
+FOUNDATION_EXPORT NSString *const StacManUnacceptedAnswerQuestionTimelineType;
+FOUNDATION_EXPORT NSString *const StacManAcceptedAnswerQuestionTimelineType;
+FOUNDATION_EXPORT NSString *const StacManVoteAggregateQuestionTimelineType;
+FOUNDATION_EXPORT NSString *const StacManRevisionQuestionTimelineType;
+FOUNDATION_EXPORT NSString *const StacManPostStateChangedQuestionTimelineType;
+
+// Revision types
+FOUNDATION_EXPORT NSString *const StacManSingleUserRevisionType;
+FOUNDATION_EXPORT NSString *const StacManVoteBasedRevisionType;
 
 // Site states
 FOUNDATION_EXPORT NSString *const StacManNormalSiteState;
 FOUNDATION_EXPORT NSString *const StacManClosedBetaSiteState;
 FOUNDATION_EXPORT NSString *const StacManOpenBetaSiteState;
 FOUNDATION_EXPORT NSString *const StacManLinkedMetaSiteState;
+
+// SuggestedEdit sorts
+FOUNDATION_EXPORT NSString *const StacManCreationSuggestedEditSort;
+FOUNDATION_EXPORT NSString *const StacManApprovalSuggestedEditSort;
+FOUNDATION_EXPORT NSString *const StacManRejectionSuggestedEditSort;
+
+// Tag periods
+FOUNDATION_EXPORT NSString *const StacManAllTimeTagPeriod;
+FOUNDATION_EXPORT NSString *const StacManMonthTagPeriod;
+
+// TagSynonym sorts
+FOUNDATION_EXPORT NSString *const StacManCreationTagSynonymSort;
+FOUNDATION_EXPORT NSString *const StacManAppliedTagSynonymSort;
+FOUNDATION_EXPORT NSString *const StacManActivityTagSynonymSort;
+
+// User sorts
+FOUNDATION_EXPORT NSString *const StacManReputationUserSort;
+FOUNDATION_EXPORT NSString *const StacManCreationUserSort;
+FOUNDATION_EXPORT NSString *const StacManNameUserSort;
+FOUNDATION_EXPORT NSString *const StacManModifiedUserSort;
+
+// UserTimline types
+FOUNDATION_EXPORT NSString *const StacManCommentedUserTimelineType;
+FOUNDATION_EXPORT NSString *const StacManAskedUserTimelineType;
+FOUNDATION_EXPORT NSString *const StacManAnsweredUserTimelineType;
+FOUNDATION_EXPORT NSString *const StacManBadgeUserTimelineType;
+FOUNDATION_EXPORT NSString *const StacManRevisionUserTimelineType;
+FOUNDATION_EXPORT NSString *const StacManAcceptedUserTimelineType;
+FOUNDATION_EXPORT NSString *const StacManReviewedUserTimelineType;
+FOUNDATION_EXPORT NSString *const StacManSuggestedUserTimelineType;
+
+// User types
+FOUNDATION_EXPORT NSString *const StacManUnregisteredUserType;
+FOUNDATION_EXPORT NSString *const StacManRegisteredUserType;
+FOUNDATION_EXPORT NSString *const StacManModeratorUserType;
+FOUNDATION_EXPORT NSString *const StacManDoesNotExistUserType;
+
+// Vote types
+FOUNDATION_EXPORT NSString *const StacManAcceptsVoteType;
+FOUNDATION_EXPORT NSString *const StacManUpVotesVoteType;
+FOUNDATION_EXPORT NSString *const StacManDownVotesVoteType;
+FOUNDATION_EXPORT NSString *const StacManBountiesOfferedVoteType;
+FOUNDATION_EXPORT NSString *const StacManBountiesWonVoteType;
+FOUNDATION_EXPORT NSString *const StacManSpamVoteType;
+FOUNDATION_EXPORT NSString *const StacManSuggestedEditsVoteType;
--- StacMan/StacManConstants.m
+++ StacMan/StacManConstants.m
@@ -8,12 +8,138 @@
 
 #import "StacManConstants.h"
 
-NSString* const StacManUnregisteredUserType = @"unregistered";
-NSString *const StacManRegisteredUserType = @"registered";
-NSString *const StacManModeratorUserType = @"moderator";
-NSString *const StacManDoesNotExistUserType = @"does_not_exist";
+NSString *const StacManActivityAnswerSort = @"activity";
+NSString *const StacManCreationAnswerSort = @"creation";
+NSString *const StacManVotesAnswerSort = @"votes";
+
+NSString *const StacManRankBadgeAllSort = @"rank";
+NSString *const StacManNameBadgeAllSort = @"name";
+NSString *const StacManTypeBadgeAllSort = @"type";
+
+NSString *const StacManGoldBadgeRank = @"gold";
+NSString *const StacManSilverBadgeRank = @"silver";
+NSString *const StacManBronzeBadgeRank = @"bronze";
+
+NSString *const StacManRankBadgeSort = @"rank";
+NSString *const StacManNameBadgeSort = @"name";
+
+NSString *const StacManNamedBadgeType = @"named";
+NSString *const StacManTagBasedBadgeType = @"tag_based";
+
+NSString *const StacManRankBadgeUserSort = @"rank";
+NSString *const StacManNameBadgeUserSort = @"name";
+NSString *const StacManTypeBadgeUserSort = @"type";
+NSString *const StacManAwardedBadgeUserSort = @"awarded";
+
+NSString *const StacManCreationCommentSort = @"creation";
+NSString *const StacManVotesCommentSort = @"votes";
+
+NSString *const StacManQuestionPostedEventType = @"question_posted";
+NSString *const StacManAnswerPostedEventType = @"answer_posted";
+NSString *const StacManCommentPostedEventType = @"comment_posted";
+NSString *const StacManPostEditedEventType = @"post_edited";
+NSString *const StacManUserCreatedEventType = @"user_created";
+
+NSString *const StacManSafeFilterType = @"safe";
+NSString *const StacManUnsafeFilterType = @"unsafe";
+NSString *const StacManInvalidFilterType = @"invalid";
+
+NSString *const StacManCommentInboxItemType = @"comment";
+NSString *const StacManChatMessageInboxItemType = @"chat_message";
+NSString *const StacManNewAnswerInboxItemType = @"new_answer";
+NSString *const StacManCareersMessageInboxItemType = @"careers_message";
+NSString *const StacManCareersInvitationInboxItemType = @"careers_invitation";
+NSString *const StacManMetaQuestionInboxItemType = @"meta_question";
+NSString *const StacManPostNoticeInboxItemType = @"post_notice";
+NSString *const StacManModeratorMessageInboxItemType = @"moderator_message";
+
+NSString *const StacManDescendingOrder = @"desc";
+NSString *const StacManAscendingOrder = @"asc";
+
+NSString *const StacManActivityPostSort = @"activity";
+NSString *const StacManCreationPostSort = @"creation";
+NSString *const StacManVotesPostSort = @"votes";
+
+NSString *const StacManQuestionPostType = @"question";
+NSString *const StacManAnswerPostType = @"answer";
+
+NSString *const StacManActivityQuestionAllSort = @"activity";
+NSString *const StacManVotesQuestionAllSort = @"votes";
+NSString *const StacManCreationQuestionAllSort = @"creation";
+NSString *const StacManHotQuestionAllSort = @"hot";
+NSString *const StacManWeekQuestionAllSort = @"week";
+NSString *const StacManMonthQuestionAllSort = @"month";
+
+NSString *const StacManActivityQuestionFavoriteSort = @"activity";
+NSString *const StacManCreationQuestionFavoriteSort = @"creation";
+NSString *const StacManVotesQuestionFavoriteSort = @"votes";
+NSString *const StacManAddedQuestionFavoriteSort = @"added";
+
+NSString *const StacManActivityQuestionRelatedSort = @"activity";
+NSString *const StacManCreationQuestionRelatedSort = @"creation";
+NSString *const StacManVotesQuestionRelatedSort = @"votes";
+NSString *const StacManRankQuestionRelatedSort = @"rank";
+
+NSString *const StacManActivityQuestionSearchSort = @"activity";
+NSString *const StacManCreationQuestionSearchSort = @"creation";
+NSString *const StacManVotesQuestionSearchSort = @"votes";
+NSString *const StacManRelevanceQuestionSearchSort = @"relevance";
+
+NSString *const StacManActivityQuestionSort = @"activity";
+NSString *const StacManCreationQuestionSort = @"creation";
+NSString *const StacManVotesQuestionSort = @"votes";
+
+NSString *const StacManQuestionQuestionTimelineType = @"question";
+NSString *const StacManAnswerQuestionTimelineType = @"answer";
+NSString *const StacManCommentQuestionTimelineType = @"comment";
+NSString *const StacManUnacceptedAnswerQuestionTimelineType = @"unaccepted_answer";
+NSString *const StacManAcceptedAnswerQuestionTimelineType = @"accepted_answer";
+NSString *const StacManVoteAggregateQuestionTimelineType = @"vote_aggregate";
+NSString *const StacManRevisionQuestionTimelineType = @"revision";
+NSString *const StacManPostStateChangedQuestionTimelineType = @"post_state_changed";
+
+NSString *const StacManSingleUserRevisionType = @"single_user";
+NSString *const StacManVoteBasedRevisionType = @"vote_based";
 
 NSString *const StacManNormalSiteState = @"normal";
 NSString *const StacManClosedBetaSiteState = @"closed_beta";
 NSString *const StacManOpenBetaSiteState = @"open_beta";
 NSString *const StacManLinkedMetaSiteState = @"linked_meta";
+
+NSString *const StacManCreationSuggestedEditSort = @"creation";
+NSString *const StacManApprovalSuggestedEditSort = @"approval";
+NSString *const StacManRejectionSuggestedEditSort = @"rejection";
+
+NSString *const StacManAllTimeTagPeriod = @"all_time";
+NSString *const StacManMonthTagPeriod = @"month";
+
+NSString *const StacManCreationTagSynonymSort = @"creation";
+NSString *const StacManAppliedTagSynonymSort = @"applied";
+NSString *const StacManActivityTagSynonymSort = @"activity";
+
+NSString *const StacManReputationUserSort = @"reputation";
+NSString *const StacManCreationUserSort = @"creation";
+NSString *const StacManNameUserSort = @"name";
+NSString *const StacManModifiedUserSort = @"modified";
+
+NSString *const StacManCommentedUserTimelineType = @"commented";
+NSString *const StacManAskedUserTimelineType = @"asked";
+NSString *const StacManAnsweredUserTimelineType = @"answered";
+NSString *const StacManBadgeUserTimelineType = @"badge";
+NSString *const StacManRevisionUserTimelineType = @"revision";
+NSString *const StacManAcceptedUserTimelineType = @"accepted";
+NSString *const StacManReviewedUserTimelineType = @"reviewed";
+NSString *const StacManSuggestedUserTimelineType = @"suggested";
+
+NSString* const StacManUnregisteredUserType = @"unregistered";
+NSString *const StacManRegisteredUserType = @"registered";
+NSString *const StacManModeratorUserType = @"moderator";
+NSString *const StacManDoesNotExistUserType = @"does_not_exist";
+
+NSString *const StacManAcceptsVoteType = @"accepts";
+NSString *const StacManUpVotesVoteType = @"up_votes";
+NSString *const StacManDownVotesVoteType = @"down_votes";
+NSString *const StacManBountiesOfferedVoteType = @"bounties_offered";
+NSString *const StacManBountiesWonVoteType = @"bounties_won";
+NSString *const StacManSpamVoteType = @"spam";
+NSString *const StacManSuggestedEditsVoteType = @"suggested_edits";
